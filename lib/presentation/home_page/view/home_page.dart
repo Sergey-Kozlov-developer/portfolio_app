b0aed5806ea8ebe588dc7d202731af117a7361bd
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2022-11-08 21:36:41 +0300

    Fixed best seller

--- lib/presentation/home_page/view/home_page.dart
+++ lib/presentation/home_page/view/home_page.dart
@@ -17,26 +17,19 @@ class HomePage extends StatelessWidget {
         elevation: 0,
         centerTitle: false,
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: SizedBox(
-              height: double.infinity,
-              child: ListView(
-                children: const [
-                  SizedBox(height: 24),
-                  BodyContentWidget(),
-                  SizedBox(height: 24),
-                  ScrollCategoryWidget(),
-                  SearchProduct(),
-                  HotSalesWidget(),
-                  BestSellerWidget(),
-                  GreedBestSellerWidget(),
-                ],
-              ),
-            ),
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: const [
+            SizedBox(height: 24),
+            BodyContentWidget(),
+            SizedBox(height: 24),
+            ScrollCategoryWidget(),
+            SearchProduct(),
+            HotSalesWidget(),
+            BestSellerWidget(),
+            GreedBestSellerWidget(),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: AppColors.bottomNavBarItem,
@@ -465,47 +458,47 @@ class GreedBestSellerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 700,
-      child: GridView.count(
-        primary: false,
-        padding: const EdgeInsets.all(20),
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        crossAxisCount: 2,
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[100],
-            child: const Text("He'd have you all unravel at the"),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[200],
-            child: const Text('Heed not the rabble'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[300],
-            child: const Text('Sound of screams but the'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[400],
-            child: const Text('Who scream'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[500],
-            child: const Text('Revolution is coming...'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[600],
-            child: const Text('Revolution, they...'),
-          ),
-        ],
-      ),
+    return GridView.count(
+      primary: false,
+      padding: const EdgeInsets.all(20),
+      crossAxisSpacing: 10,
+      mainAxisSpacing: 10,
+      crossAxisCount: 2,
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      scrollDirection: Axis.vertical,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[100],
+          child: const Text("He'd have you all unravel at the"),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[200],
+          child: const Text('Heed not the rabble'),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[300],
+          child: const Text('Sound of screams but the'),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[400],
+          child: const Text('Who scream'),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[500],
+          child: const Text('Revolution is coming...'),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.teal[600],
+          child: const Text('Revolution, they...'),
+        ),
+      ],
     );
   }
 }
