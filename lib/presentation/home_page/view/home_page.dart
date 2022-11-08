2a6c4579288ffe757910c6e4e9968c563facb407
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2022-11-08 15:33:42 +0300

    Added hot sales

--- lib/presentation/home_page/view/home_page.dart
+++ lib/presentation/home_page/view/home_page.dart
@@ -17,13 +17,18 @@ class HomePage extends StatelessWidget {
         elevation: 0,
         centerTitle: false,
       ),
-      body: Column(
-        children: const [
-          SizedBox(height: 24),
-          BodyContentWidget(),
-          SizedBox(height: 24),
-          ScrollCategoryWidget(),
-          SearchProduct(),
+      body: ListView(
+        children: [
+          Column(
+            children: const [
+              SizedBox(height: 24),
+              BodyContentWidget(),
+              SizedBox(height: 24),
+              ScrollCategoryWidget(),
+              SearchProduct(),
+              HotSalesWidget(),
+            ],
+          ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -171,12 +176,14 @@ class ScrollCategoryWidget extends StatelessWidget {
               icon: Image.asset(AppIcons.health),
             ),
             color: AppColors.whiteColor,
-            text: const Text('Health',
+            text: const Text(
+              'Health',
               style: TextStyle(
                 fontSize: 12,
                 fontWeight: FontWeight.w500,
                 color: AppColors.bottomNavBarItem,
-              ),),
+              ),
+            ),
           ),
           CategoryIconWidget(
             iconButton: IconButton(
@@ -184,12 +191,14 @@ class ScrollCategoryWidget extends StatelessWidget {
               icon: Image.asset(AppIcons.books),
             ),
             color: AppColors.whiteColor,
-            text: const Text('Books',
+            text: const Text(
+              'Books',
               style: TextStyle(
                 fontSize: 12,
                 fontWeight: FontWeight.w500,
                 color: AppColors.bottomNavBarItem,
-              ),),
+              ),
+            ),
           ),
           CategoryIconWidget(
             iconButton: IconButton(
@@ -197,12 +206,14 @@ class ScrollCategoryWidget extends StatelessWidget {
               icon: Image.asset(AppIcons.books),
             ),
             color: AppColors.whiteColor,
-            text: const Text('Books',
+            text: const Text(
+              'Books',
               style: TextStyle(
                 fontSize: 12,
                 fontWeight: FontWeight.w500,
                 color: AppColors.bottomNavBarItem,
-              ),),
+              ),
+            ),
           ),
         ],
       ),
@@ -259,6 +270,131 @@ class SearchProduct extends StatelessWidget {
             icon: Image.asset(AppIcons.menu),
           ),
         ),
+      ],
+    );
+  }
+}
+
+class HotSalesWidget extends StatelessWidget {
+  const HotSalesWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Hot sales',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 25,
+                color: AppColors.bottomNavBarItem,
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'see more',
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 15,
+                  color: AppColors.redColor,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Stack(
+          alignment: Alignment.topLeft,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              height: 220,
+              width: 500,
+              padding: const EdgeInsets.only(left: 21, right: 27),
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Image.network(
+                      'https://img.ibxk.com.br/2020/09/23/23104013057475.jpg?w=1120&h=420&mode=crop&scale=both'),
+                  // const SizedBox(width: 10),
+                  // Image.network(
+                  //     'https://img.ibxk.com.br/2020/09/23/23104013057475.jpg?w=1120&h=420&mode=crop&scale=both'),
+                ],
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 35,
+                  height: 35,
+                  margin: EdgeInsets.only(left: 44, top: 12),
+                  // color: AppColors.redColor,
+                  decoration: const BoxDecoration(
+                    color: AppColors.redColor,
+                    borderRadius: const BorderRadius.all(Radius.circular(35)),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'New',
+                      style: TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.w700,
+                        color: AppColors.whiteColor,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 12),
+                const Padding(
+                  padding: EdgeInsets.only(left: 44),
+                  child: Text(
+                    'IPhone 12',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w700,
+                      color: AppColors.whiteColor,
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 44, top: 5),
+                  child: Text(
+                    'Súper. Mega. Rápido.',
+                    style: TextStyle(
+                      fontSize: 11,
+                      fontWeight: FontWeight.w400,
+                      color: AppColors.whiteColor,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 44, top: 31),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: const ButtonStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll<Color>(AppColors.whiteColor),
+                    ),
+                    child: const Text(
+                      'Buy now!',
+                      style: TextStyle(
+                        fontSize: 11,
+                        fontWeight: FontWeight.w700,
+                        color: AppColors.bottomNavBarItem,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
       ],
     );
   }
