e7675618231de9306ea1556a82d7739742813949
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2022-11-17 20:49:21 +0300

    Hot fix dio

--- lib/presentation/home_page/widget/greed_best_seller_widget.dart
+++ lib/presentation/home_page/widget/greed_best_seller_widget.dart
@@ -11,48 +11,45 @@ class GreedBestSellerWidget extends StatefulWidget {
 }
 
 class _GreedBestSellerWidgetState extends State<GreedBestSellerWidget> {
-
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     final double itemHeight = (size.height - kToolbarHeight - 138) / 2;
-    final double itemWidth = size.width / 2.1;
-    return BlocBuilder<HomeBloc, HomeState>(
-        builder: (context, state) {
-          if (state is HomeLoadingState) {
-            return const Center(
-                child: CircularProgressIndicator()
-            );
-          }
-          if (state is HomeLoadedState) {
-            return GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: (itemWidth / itemHeight),),
-                primary: false,
-                padding: const EdgeInsets.all(20),
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                scrollDirection: Axis.vertical,
-                itemCount: state.loadedBestseller.length,
-              itemBuilder: (context, index) => GridWidget(
-                  pictureUrl: state.loadedBestseller[index].picture,
-                  titleItems: state.loadedBestseller[index].title,
-                  priceWithoutDiscount: state.loadedBestseller[index].priceWithoutDiscount,
-                  discountPrice: state.loadedBestseller[index].discountPrice,
-                  isFavorites: state.loadedBestseller[index].isFavorites,
-                  id: state.loadedBestseller[index].id
-
-              ),
-                );
-          }
-          if (state is HomeErrorState) {
-            return Text('Error');
-          }
-          return CircularProgressIndicator();
-        });
+    final double itemWidth = size.width / 1.5;
+    return BlocBuilder<HomeBloc, HomeState>(builder: (context, state) {
+      if (state is HomeLoadingState) {
+        return const Center(child: CircularProgressIndicator());
+      }
+      if (state is HomeLoadedState) {
+        return GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: (itemWidth / itemHeight),
+          ),
+          primary: false,
+          padding: const EdgeInsets.all(20),
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
+          itemCount: state.loadedBestseller.length,
+          itemBuilder: (context, index) => GridWidget(
+              pictureUrl: state.loadedBestseller[index].picture,
+              titleItems: state.loadedBestseller[index].title,
+              priceWithoutDiscount:
+                  state.loadedBestseller[index].priceWithoutDiscount,
+              discountPrice: state.loadedBestseller[index].discountPrice,
+              isFavorites: state.loadedBestseller[index].isFavorites,
+              id: state.loadedBestseller[index].id),
+        );
+      }
+      if (state is HomeErrorState) {
+        return Text('Error');
+      }
+      return CircularProgressIndicator();
+    });
   }
 }
+
 class GridWidget extends StatelessWidget {
   final bool? isFavorites;
   final String? titleItems;
@@ -60,7 +57,6 @@ class GridWidget extends StatelessWidget {
   final int? discountPrice;
   final String? pictureUrl;
   final int? id;
-
 
   const GridWidget({
     Key? key,
@@ -89,77 +85,66 @@ class GridWidget extends StatelessWidget {
             //   MaterialPageRoute(builder: (context) => const ProductDetailsPage()),
             // );
           },
-          child: Stack(
+          child: Stack(alignment: Alignment.topCenter, children: [
+            SizedBox(
+                child: Image.network(
+              pictureUrl ?? '',
+              height: 190,
+              width: double.maxFinite,
+              fit: BoxFit.cover,
               alignment: Alignment.topCenter,
-              children: [
-                SizedBox(
-                  child: Image.network(
-                    pictureUrl ?? '',
-                    height: 180,
-                    // width: double.maxFinite,
-                    fit: BoxFit.cover,
-                    // alignment: Alignment.topLeft,
-                  )
-                ),
-                Positioned(
-                  right: 10,
-                  child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                          shape: const CircleBorder(),
-                          padding: const EdgeInsets.all(5),
-                          primary: Colors.white,
-                          minimumSize: const Size(15, 15)
-                      ),
-                      child: isFavorites == true
-                          ? const Icon(Icons.favorite, color: Colors.red, size: 15)
-                          : const Icon(Icons.favorite_border, color: Colors.red, size: 15)
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.end,
+            )),
+            Positioned(
+              right: 10,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(5),
+                      primary: Colors.white,
+                      minimumSize: const Size(15, 15)),
+                  child: isFavorites == true
+                      ? const Icon(Icons.favorite, color: Colors.red, size: 15)
+                      : const Icon(Icons.favorite_border,
+                          color: Colors.red, size: 15)),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('\$' + priceWithoutDiscount.toString(),
-                              style: const TextStyle(
-                                  fontFamily: 'MarkProbold',
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w700,
-                                  color: AppColors.bottomNavBarItem,
-                              )
-                          ),
-                          const SizedBox(width: 20),
-                          Text('\$' + discountPrice.toString(),
-                              style: TextStyle(
-                                  fontFamily: 'MarkPronormal400',
-                                  fontSize: 11,
-                                  fontWeight: FontWeight.w700,
-                                  color: Colors.grey[400],
-                                  decoration: TextDecoration.lineThrough
-                              )
-                          ),
-                        ],
-                      ),
-                      Text(
-                          titleItems ?? '',
+                      Text('\$' + priceWithoutDiscount.toString(),
                           style: const TextStyle(
+                            fontFamily: 'MarkProbold',
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                            color: AppColors.bottomNavBarItem,
+                          )),
+                      const SizedBox(width: 20),
+                      Text('\$' + discountPrice.toString(),
+                          style: TextStyle(
                               fontFamily: 'MarkPronormal400',
                               fontSize: 11,
                               fontWeight: FontWeight.w700,
-                            color: AppColors.bottomNavBarItem,
-                          )
-                      ),
+                              color: Colors.grey[400],
+                              decoration: TextDecoration.lineThrough)),
                     ],
                   ),
-                ),
-              ]
-          ),
-        )
-    );
+                  Text(titleItems ?? '',
+                      style: const TextStyle(
+                        fontFamily: 'MarkPronormal400',
+                        fontSize: 11,
+                        fontWeight: FontWeight.w700,
+                        color: AppColors.bottomNavBarItem,
+                      )),
+                ],
+              ),
+            ),
+          ]),
+        ));
   }
 }
